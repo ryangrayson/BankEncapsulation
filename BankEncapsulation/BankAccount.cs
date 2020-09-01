42f072a1f008d3ea1ff262eeb8554f3ee507536a
Ryan Grayson <ryan.g.grayson@gmail.com>
2020-09-01 18:15:25 -0500

    Exercise finished, but would like to elaborate

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        //Create a private field of type double named balance with a value of 0
+        private double balance = 0; //"balance" is what you're trying to protect, or encapsulate
+
+        //Define a method named Deposit that will accept a double and store that value in the balance field
+
+
+        //Encapsulation
+        public void Deposit(double amount) //double because it could be a decimal
+                                           //amount because it makes sense to name as the amount of the deposit 
+        {
+            balance = amount;
+        }
+
+
+        //Define a method named GetBalance that will return the amount stored in the balance field
+        public double GetBalance()        
+        {
+            return balance;             //Can work with balance here because it is inside the class it was assigned
+        }
+
+        
+       
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the main method of your application, create a new instance of the BankAccount class
+            var ryansAccount = new BankAccount(); //--How to create a new instance of class BankAccount
+
+            //Allow the user of the application to Deposit money and retrieve their balance through the console
+            Console.WriteLine("Hello! How much would you like to deposit?");
+            
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            //Call the deposit method
+            ryansAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! You have deposited ${amountToDeposit}\n" +
+                $"Your balance is now ${ryansAccount.GetBalance()}");
+            Console.Beep();
+
+            Console.WriteLine("Would you like to make another deposit?\n" +
+                "Type Y or N");
+            
+
+        }
+    }
+}
